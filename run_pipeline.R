696af44632dbc4b106439fdc06bf269cce990e5d
oppy-github <56025990+oppy-github@users.noreply.github.com>
2026-01-30 18:45:37 +1030

    data pipeline

--- run_pipeline.R
+++ run_pipeline.R
@@ -4,7 +4,7 @@ scripts <- c(
   "R/clean",
   "R/features",
   "R/Compute quartiles and IQR",
-  "R/Correlation with Significance Te...",
+  "R/Correlation with Significance Testing",
   "R/RFM Segmentation",
   "R/Clustering",
   "R/Visualization"
